30852839c08a09059355f07bc0bbdb4ed85ffb63
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2021-11-09 08:27:37 -0500

    Added modal popover when submitting an item to add.

---
+++ TestProd/AddItemViewController.swift
@@ -0,0 +1,56 @@
+//
+//  AddItemViewController.swift
+//  TestProd
+//
+//  Created by Chris Rinaldi on 11/9/21.
+//
+
+import UIKit
+import FirebaseFirestore
+import FirebaseAuth
+
+class AddItemViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var itemInputField: UITextField!;
+    
+    @IBOutlet weak var itemDescriptionField: UITextField!;
+    
+    @IBOutlet weak var itemValueField: UITextField!;
+    
+    var db: Firestore!
+    
+    
+    //MARK: Handlers
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    //This function is called when the user presses the add item button. Performs logic
+    //to submit data to Firestore.
+    @IBAction func didPressAddItem(_semder: Any) {
+                let item = itemInputField!.text
+                let description = itemDescriptionField!.text
+                let value = itemValueField!.text
+                // Add a new document with a generated ID
+                var ref: DocumentReference? = nil
+                ref = db.collection("inventory-log").addDocument(data: [
+                    "name": item,
+                    "id": 0,
+                    "value": value,
+                    "date": "today",
+                    "sender": Auth.auth().currentUser?.email,
+                ]) { err in
+                    if let err = err {
+                        print("Error adding document: \(err)")
+                    } else {
+                        print("Document added with ID: \(ref!.documentID)")
+//                        self.messageInputField.text = ""
+                    }
+                }
+    }
+    
+    //MARK: Private
+    
+}
